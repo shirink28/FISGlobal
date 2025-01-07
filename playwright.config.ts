bd9a8abe9d8b3c2338f7f85098b1e5a82d16415b
Shirin <shirin28990@gmail.com>
2025-01-07 23:10:11 +0100

    first commit

---
+++ playwright.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+export default defineConfig({
+  testDir: './tests',
+  /* Opt out of parallel tests. */
+  workers:1,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+ 
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], 
+        headless:false,
+        channel: 'chrome',
+        deviceScaleFactor:undefined,
+        viewport:null,
+      launchOptions:{
+        args:['--start-maximized'],
+      } },
+    },
+  ],
+
+ 
+});
